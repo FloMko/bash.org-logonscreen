3fb99a910451b067a47696b686bbb2b712cf5200
flomko <flomko.ru@gmail.com>
2017-02-06 00:42:12 +0300

    no know how to use span from stdout

--- sceleton/logon/parser.py
+++ sceleton/logon/parser.py
@@ -5,9 +5,16 @@ Created on 2017-02-05
 @author: flomko
 '''
 import requests
+import re
 
-def init():
+def question():
 	data = requests.get ('http://bash.im/forweb/?u')
 	answer = data.text
 	print (answer)
-init()
+#def cut_text():
+	pos_start = re.search('1em 0;">',answer)
+	pos_end = re.search('small>',answer)
+	html_text = [pos_start,pos_end]
+	print (html_text)
+question()
+#cut_text()
